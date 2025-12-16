fdf838c404d8fa9c9b937746caf10a42045c62f4
Anas Imran <anamiimran101nb@gmail.com>
2025-12-16 22:05:09 +0500

    resolve syntax erros

---
+++ src/skipgram.cu
@@ -0,0 +1,5 @@
+#include <iostream>
+void trainGpu(int senetence_index) {};
+void getResultData();
+void initGpu();
+void freeGpu();
--- src/word2vec.cpp
+++ src/word2vec.cpp
@@ -8,19 +8,23 @@
 #include <vector>
 #include <unordered_map>
 #include <map>
+#include <skipgram.h>
+
+#define MAX_SENTENCE_LENGTH 10
+#define MAX_SENTENCE_NUM 10
 
 std::unordered_map<std::string, int> vocab_hash;
-std::vector<word> vocab_list;
+std::vector<word_index> vocab_list;
 
 std::string train_corpus_file, output_file;
 std::string save_vocab_file, read_vocab_file;
-struct word *vocab;
+struct word_index *vocab;
 std::ifstream fin;
 
 int window = 5, min_count = 5, num_threads = 12, min_reduce = 1;
 
 size_t vocab_size = 0, layer1_size = 100, layer1_size_aligned;
-;
+
 long long train_words = 0, word_count_actual = 0, file_size = 0;
 int epochs = 5;
 double alpha = 0.025, starting_alpha, sample = 1e-3;
@@ -45,7 +49,7 @@ void initUnigramDistribuiton()
         p_w += pow(vocab_list[i].count, a) / sum_p;
         prev_index = index;
         index = p_w * table_size;
-        std::cout << "Word: " << vocab_list[i].word << " Prob: " << p_w << " Index: " << index << std::endl;
+        std::cout << "Word: " << vocab_list[i].word_index << " Prob: " << p_w << " Index: " << index << std::endl;
         for (size_t j = prev_index; j < index && j < table_size; j++)
         {
             table[j] = i;
@@ -55,17 +59,20 @@ void initUnigramDistribuiton()
         table[index++] = vocab_list.size() - 1;
 }
 
+void InitNet();
+
+
 void *TrainModelThread(void *id)
 {
-    int word, sentence_length = 0;
+    int word_index, sentence_length = 0;
     long long word_count = 0, last_word_count = 0;
-    int local_iter = iter;
+    int local_iter = epochs; //check
     unsigned int next_random = (long)id;
     int sentence_num;
     clock_t now;
     float *alpha_ptr = (float *)sen + MAX_SENTENCE_NUM * MAX_SENTENCE_LENGTH;
-    FILE *fi = fopen(train_file, "rb");
-    fseek(fi, file_size / (int)num_threads * (long)id, SEEK_SET);
+    std::ifstream fi(train_corpus_file, std::ios::in);
+    fi.seekg(std::ios::pos_type(file_size / (int)num_threads * (long)id));
     sentence_length = 0;
     sentence_num = 0;
     while (1)
@@ -74,38 +81,37 @@ void *TrainModelThread(void *id)
         {
             word_count_actual += word_count - last_word_count;
             last_word_count = word_count;
-            if ((debug_mode > 1))
-            {
-                now = clock();
-                printf("%cAlpha: %f  Progress: %.2f%%  Words/thread/sec: %.2fk  ", 13, alpha,
-                       word_count_actual / (float)(iter * train_words + 1) * 100,
-                       word_count_actual / ((float)(now - start + 1) / (float)CLOCKS_PER_SEC * 1000));
-                fflush(stdout);
-            }
-            alpha = starting_alpha * (1 - word_count_actual / (float)(iter * train_words + 1));
+            now = clock();
+            printf("%cAlpha: %f  Progress: %.2f%%  Words/thread/sec: %.2fk  ", 13, alpha,
+                    word_count_actual / (float)(epochs * train_words + 1) * 100,
+                    word_count_actual / ((float)(now - start + 1) / (float)CLOCKS_PER_SEC * 1000));
+            std::cout.flush();
+            alpha = starting_alpha * (1 - word_count_actual / (float)(epochs * train_words + 1));
             if (alpha < starting_alpha * 0.0001)
                 alpha = starting_alpha * 0.0001;
         }
-
+        std::string word;
+        int word_index;
         while (1)
         {
-            word = ReadWordIndex(fi);
-            if (feof(fi))
+            loadWordFromFile(word, fi);
+            word_index = getWordIndex(word); 
+            if (fi.eof())
                 break;
-            if (word == -1)
+            if (word_index == -1)
                 continue;
             word_count++;
-            if (word == 0)
+            if (word_index == 0)
                 break;
             // The subsampling randomly discards frequent words while keeping the ranking same
             if (sample > 0)
             {
-                float ran = (sqrt(vocab[word].cn / (sample * train_words)) + 1) * (sample * train_words) / vocab[word].cn;
+                float ran = (sqrt(vocab_list[word_index].count / (sample * train_words)) + 1) * (sample * train_words) / vocab_list[word_index].count;
                 next_random = next_random * (unsigned int)1664525 + 1013904223;
                 if (ran < (next_random & 0xFFFF) / (float)65536)
                     continue;
             }
-            sen[sentence_num * MAX_SENTENCE_LENGTH + sentence_length] = word;
+            sen[sentence_num * MAX_SENTENCE_LENGTH + sentence_length] = word_index;
             sentence_length++;
             if (sentence_length >= MAX_SENTENCE_LENGTH)
             {
@@ -118,12 +124,12 @@ void *TrainModelThread(void *id)
         }
 
         // Do GPU training here
-        TrainGPU(sentence_num);
+        trainGpu(sentence_num);
         //////////////////////
         sentence_num = 0;
         sentence_length = 0;
 
-        if (feof(fi) || (word_count > train_words / num_threads))
+        if (fi.eof() || (word_count > train_words / num_threads))
         {
             word_count_actual += word_count - last_word_count;
             local_iter--;
@@ -131,18 +137,17 @@ void *TrainModelThread(void *id)
                 break;
             word_count = 0;
             last_word_count = 0;
-            fseek(fi, file_size / (int)num_threads * (long)id, SEEK_SET);
+            fi.seekg(std::ios::pos_type(file_size / (int)num_threads * (long)id));
         }
     }
-    GetResultData();
-    fclose(fi);
+    getResultData();
+    fi.close();
     pthread_exit(NULL);
 }
 
 void TrainModel()
 {
     long a, b, c, d;
-    FILE *fo;
     pthread_t *pt = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
     std::cout << "Starting training using file " << train_corpus_file << std::endl;
     starting_alpha = alpha;
@@ -157,21 +162,22 @@ void TrainModel()
     InitNet();
     
     initUnigramDistribuiton();
-    initializeGPU();
+    initGpu();
     start = clock();
     for (a = 0; a < num_threads; a++)
         pthread_create(&pt[a], NULL, TrainModelThread, (void *)a);
     for (a = 0; a < num_threads; a++)
         pthread_join(pt[a], NULL);
-    cleanUpGPU();
-    // Save the word vectors
+    freeGpu();
+    // Save the word_index vectors
 
     std::ofstream out(output_file, binary ? std::ios::binary : std::ios::out);
     
-    fprintf(fo, "%d %d\n", vocab_size, layer1_size);
+
+    out << vocab_size << " " << layer1_size << std::endl;
     for (size_t i = 0; i < vocab_size; i++)
     {
-        fprintf(fo, "%s ", vocab[i].word);
+        out >> vocab_list[i] << " ";
         if (binary)
             for (b = 0; b < layer1_size; b++)
                 out << syn0[i * layer1_size_aligned + b] << " ";
